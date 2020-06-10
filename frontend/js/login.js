dd2b67d33b2847953f53edc43db40c6832fcf6a6
wonderthang12 <44790720+wonderthang12@users.noreply.github.com>
2020-06-10 11:50:31 +0700

    uytyt
    uuyuu

---
+++ frontend/js/login.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//     $("#login").click(function(){
+//         var email = $("#email").val().trim();
+//         var password = $("#password").val.trim();
+//         if(email = "admin01@gmail.com" && password = "123456"){
+//             window.location = "../html/admin.html";
+//         }
+//     });
+// });
